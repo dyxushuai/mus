fd831cff52412c286ef58ec46595996f22b8e295
xus <dyxushuai@gmail.com>
2015-01-04 15:12:52 +0800

    add some methods

--- config/config.go
+++
@@ -1,7 +0,0 @@
-package config
-
-const (
-	PREFIX = "mus:"
-	REDIS_SERVER = "example.com:8000"
-	REDIS_PASSWORD = "123456"
-)
--- manager/redis.go
+++ manager/redis.go
@@ -3,12 +3,21 @@ package manager
 import (
 	"github.com/garyburd/redigo/redis"
 	"time"
+	"fmt"
 )
+
+const (
+	PREFIX = "mus:"
+	Hour = iota
+	Day
+	Month
+	Year
+)
+
 
 type Storage struct {
 	pool *redis.Pool
 }
-
 
 
 func NewStorage(server, password string) (s *Storage) {
@@ -43,19 +52,43 @@ func (self * Storage) withConnDo(arg... interface {}) (reply interface{}, err er
 	return
 }
 
+func (self *Storage) getKeyBy(k int) (key string) {
+
+	var year, month, day, hour int
+	now := time.Now()
+	year = now.Year()
+	month = int(now.Month())
+	day = now.Day()
+	hour = now.Hour()
+
+	switch k {
+	case Hour:
+		key = fmt.Sprintf("%s:%d:%d:%d:%d", self.Port, year, month, day, hour)
+	case Day:
+		key = fmt.Sprintf("%s:%d:%d:%d", self.Port, year, month, day)
+	case Month:
+		key = fmt.Sprintf("%s:%d:%d", self.Port, year, month)
+	case Year:
+		key = fmt.Sprintf("%s:%d", self.Port, year)
+	default:
+
+	}
+	return
+}
+
 func (self *Storage) Keys(pat string) (data []byte, err error) {
 	var data []byte
 	data, err = redis.Bytes(self.withConnDo("KEYS", PREFIX + pat))
 	return
 }
 
-func (self *Storage) GetBy(key string) (data []byte, err error) {
+func (self *Storage) GetServer(key string) (data []byte, err error) {
 	var data []byte
 	data, err = redis.Bytes(self.withConnDo("GET", PREFIX + key))
 	return
 }
 
-func (self *Storage) SetBy(key string, data []byte) (err error) {
+func (self *Storage) SetServer(key string, data []byte) (err error) {
 	_, err = self.withConnDo("SET", key, data)
 	return
 }
@@ -70,3 +103,38 @@ func (self *Storage) GetSize(key string) (score int64, err error) {
 	return
 }
 
+func (self *Storage) IncreaseByHour(incr int) (score int64, err error) {
+	key := self.getKeyBy(Hour)
+	if key != "" {
+		return
+	}
+	score, err = self.IncrSize(key, incr)
+	return
+}
+
+func (self *Storage) IncreaseByDay(incr int) (score int64, err error) {
+	key := self.getKeyBy(Day)
+	if key != "" {
+		return
+	}
+	score, err = self.IncrSize(key, incr)
+	return
+}
+
+func (self *Storage) IncreaseByMonth(incr int) (score int64, err error) {
+	key := self.getKeyBy(Month)
+	if key != "" {
+		return
+	}
+	score, err = self.IncrSize(key, incr)
+	return
+}
+
+func (self *Storage) IncreaseByYear(incr int) (score int64, err error) {
+	key := self.getKeyBy(Year)
+	if key != "" {
+		return
+	}
+	score, err = self.IncrSize(key, incr)
+	return
+}
